17ff60bd46abe7df10a39d05ac8dea966bde4fcc
Ricky Codes <rickydoescode@gmail.com>
2020-06-25 10:37:31 +0700

    added boilerplate code

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Hi"),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
